7ed8825bec034f002c0c85291e99f30d2d531a84
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-09-21 23:54:45 +0100

    chore: update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,32 +1,40 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.normalize(path.join(path.dirname(__dirname) + "/../.env")) });
+dotenv.config({
+  path: path.normalize(path.join(path.dirname(__dirname) + "/../.env")),
+});
 
 const options = {
-    development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        app_port: process.env.PORT,
-    },
-    test: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        app_port: process.env.PORT,
-    },
-    production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        app_port: process.env.PORT,
-    },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    app_port: process.env.PORT,
+    url: process.env.BASE_URL,
+    pattern: process.env.PATTERN,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    app_port: process.env.PORT,
+    url: process.env.BASE_URL,
+    pattern: process.env.PATTERN,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    app_port: process.env.PORT,
+    url: process.env.BASE_URL,
+    pattern: process.env.PATTERN,
+  },
 };
 
 const env = process.env.NODE_ENV;
