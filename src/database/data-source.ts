d5e8a13a4d77e59dd96ca39c6ccd6a1db37f7105
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-09-21 23:55:54 +0100

    fix: proper linting

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,15 @@
-import {config} from "../config/config";
-import {Item} from "./entity/Item";
-import {createItems1663609015510} from "./migrations/1663609015510-create-items";
+import { config } from "../config/config";
 import { DataSource } from "typeorm";
 
-
 export const DBSource = new DataSource({
-    type: "postgres",
-    host: config.host,
-    port: parseInt(config.port),
-    username: config.username,
-    password: config.password,
-    database: config.database,
-    synchronize: false,
-    logging: false,
-    entities: [Item],
-    migrations: [createItems1663609015510],
+  type: "postgres",
+  host: config.host,
+  port: parseInt(config.port),
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  synchronize: false,
+  logging: false,
+  entities: [__dirname + "/entity/*.{ts,js}"],
+  migrations: [__dirname + "/migrations/*.{ts,js}"],
 });
--- src/database/entity/Item.ts
+++ src/database/entity/Item.ts
@@ -1,23 +1,22 @@
 import { Column, Entity, BaseEntity, PrimaryGeneratedColumn } from "typeorm";
 
-
 @Entity("items")
 export class Item extends BaseEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id!: string
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
 
-    @Column()
-    hackerId!: number;
+  @Column()
+  hackerId!: number;
 
-    @Column()
-    title?: string
+  @Column()
+  title?: string;
 
-    @Column({type: "boolean", default: true})
-    byHackerNews!: boolean
+  @Column({ type: "boolean", default: true })
+  byHackerNews!: boolean;
 
-    @Column()
-    text?: string
+  @Column()
+  text?: string;
 
-    @Column()
-    url?: string
+  @Column()
+  url?: string;
 }
