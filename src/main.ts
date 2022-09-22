1c22bddc6dc3d2ba7897b97510a3b2e7b179cfae
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-09-22 04:39:25 +0100

    feat: app start-up

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { DBSource } from "./database/data-source";
+import express from "express";
+import { config } from "./config/config";
+import { router } from "./routes/item.route";
 import { pollWorker } from "./worker/scheduler";
+
+const app = express();
+const port = config.app_port;
+app.use(express.json());
+app.use("/items", router);
 
 DBSource.initialize()
   .then(() => {
@@ -8,5 +16,9 @@ DBSource.initialize()
   .catch((err) => {
     console.error(`Data Source initialization error`, err);
   });
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
 
 pollWorker.run();
