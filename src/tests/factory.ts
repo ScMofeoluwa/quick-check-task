695f265acb59ff22ccc2864b955acc491004c5f3
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2022-09-22 23:18:49 +0100

    feat: factory girl to set up test data

--- src/tests/factory.ts
+++ src/tests/factory.ts
@@ -6,12 +6,12 @@ const newsType = ["job", "story", "comment", "poll", "pollopt"];
 //TODO: TypeORM adapter for Factory-Girl
 
 factory.define("Item", Item, {
-  hackerId: factory.chance("natural", { min: 10000000, max: 99999999 }),
-  title: factory.chance("sentence", { words: 5 }),
-  byHackerNews: false,
-  text: factory.chance("paragraph", { sentences: 3 }),
-  url: factory.chance("url"),
-  type: factory.sequence("item.type", (n) => newsType[n % 5]),
+    hackerId: factory.chance("natural", { min: 10000000, max: 99999999 }),
+    title: factory.chance("sentence", { words: 5 }),
+    byHackerNews: false,
+    text: factory.chance("paragraph", { sentences: 3 }),
+    url: factory.chance("url"),
+    type: factory.sequence("item.type", (n) => newsType[n % 5]),
 });
 
 export { factory };
